1f826553697143e409c2074b045dce6085432466
DamianLikszo <damian.likszo@gmail.com>
2019-01-13 23:32:46 +0100

    add swagger

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using StarChart.Data;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace StarChart
 {
@@ -13,12 +14,18 @@ namespace StarChart
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("StartChart"));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("StarChart"));
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info { Title = "StarChart Web API", Version = "v1" }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "StarChart Web API");
+            });
             app.UseMvc();
         }
     }
